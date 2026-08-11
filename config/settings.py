d43980dda6943ad81325b9d5f46f1afb1640f297
ntfray <rayofdetermination@mail.ru>
2026-08-11 16:32:52 +0300

    Add new env var DJANGO_DEBUG for setting.py

--- config/settings.py
+++ config/settings.py
@@ -20,7 +20,7 @@ STATICFILES_DIRS = [
 SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv('DJANGO_DEBUG')
 
 ALLOWED_HOSTS = [
     'localhost',
